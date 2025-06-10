49848dce9dca4f5e307ffb8ec4e0962f1247c1b2
Andres Aguilar <aguilarcarboni@gmail.com>
2025-06-09 18:35:49 -0600

    initial commit

---
+++ financesTests/financesTests.swift
@@ -0,0 +1,16 @@
+//
+//  financesTests.swift
+//  financesTests
+//
+//  Created by Andrés on 9/6/2025.
+//
+
+import Testing
+
+struct financesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
